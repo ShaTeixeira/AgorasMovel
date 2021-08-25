9d2ed0eb11fdeb2b7e2d068b9c7bb40acd46baf1
IAteYourKookie <loorenachan@gmail.com>
2021-08-25 10:55:05 -0300

    att o arquivo toolbar pronto

--- app/src/main/java/com/example/agorasmovel/MainActivity.java
+++ app/src/main/java/com/example/agorasmovel/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.agorasmovel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.perfil:
+                //caso o botao perfil seja selecionado na toolbar
+                return true;
+
+            case R.id.tema:
+                //caso sugestao de tema
+                return true;
+
+            case R.id.oldDebate:
+                //debates antigos
+                return true;
+
+            case R.id.ajuda:
+                //ajuda
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
