21a64bd6df414507646db34e0a9a3031435d370d
IAteYourKookie <loorenachan@gmail.com>
2022-01-22 22:14:18 -0300

    Dando erro

--- app/src/main/java/com/example/agorasmovel/EditPerfilActivity.java
+++ app/src/main/java/com/example/agorasmovel/EditPerfilActivity.java
@@ -1,13 +1,22 @@
 package com.example.agorasmovel;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -30,15 +39,33 @@ import java.util.concurrent.Executors;
 
 public class EditPerfilActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST =0;
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    ImageView imageView = findViewById(R.id.imgEditPhoto);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_REQUEST);
+        }
+
+        ImageView imgEditPhoto = findViewById(R.id.imgEditPhoto);
+        imgEditPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editperfil);
 
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
 
-        ImageButton imgEditPhoto = findViewById(R.id.imgEditPhoto);
         PhotoViewModel photoViewModel = new ViewModelProvider(this).get(PhotoViewModel.class);
         String currentPhotoPath = photoViewModel.getCurrentPhotoPath();
         //verificar se tem foto
@@ -173,6 +200,39 @@ public class EditPerfilActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_REQUEST:
+                if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"Permissões concedidas", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this,"Permissões não concedidas", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESULT_LOAD_IMAGE:
+                if(resultCode==RESULT_OK){
+                    Uri selectedImage=data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedImage,filePathColumn,null,null,null,null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+                }
+        }
     }
 
     @Override
